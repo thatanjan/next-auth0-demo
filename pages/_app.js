7f5bc121210ab4b3e4f3bbcd1c0494c876832324
Anjan Shomodder <thatanjan@gmail.com>
2022-10-16 17:32:22 +0600

    Starter code done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { UserProvider } from '@auth0/nextjs-auth0'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<UserProvider>
+			<ChakraProvider>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</UserProvider>
+	)
 }
 
 export default MyApp
